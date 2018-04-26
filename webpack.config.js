61cf3bbbb3c2a2c99709e830b1dcb954a52b47cb
nihaoye <2232737508@qq.com>
2018-04-26 11:38:02 +0800

    删除一些文件，自己编译出来吧

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@ module.exports = {
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'fc3dmap.js'
+    },
+    module: {
+        rules: [ // 用于规定在不同模块被创建时如何处理模块的规则数组
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+
+                },
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, 'src'),
+            }
+        ]
     }
 };
